70e62dc312cc78a45a608804bca352c83460db8e
Vipul Soni <sonivipul89@gmail.com>
2025-08-17 21:10:31 +0530

    mobile specific enhancement

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Global Blog Space — Fresh perspectives, fast.';
+export const SITE_TITLE = 'Global Blog Space';
+export const TAG_LINE = 'Fresh perspectives, fast.'
 export const SITE_DESCRIPTION = 'Welcome to Global Blog Space a hub where bloggers share stories, ideas, and knowledge without boundaries.';
