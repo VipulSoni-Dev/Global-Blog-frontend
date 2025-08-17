1a9ad7d76e4acfd44006bedda638aa04632cc48e
Vipul Soni <sonivipul89@gmail.com>
2025-08-17 17:36:15 +0530

    SEO optimization for global blog space keyword for header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Global-Blog — Fresh perspectives, fast.';
-export const SITE_DESCRIPTION = 'Global-Blog: crisp, fast, and focused writing on software, data, and the web.';
+export const SITE_TITLE = 'Global Blog Space — Fresh perspectives, fast.';
+export const SITE_DESCRIPTION = 'Welcome to Global Blog Space a hub where bloggers share stories, ideas, and knowledge without boundaries.';
